9d13b0e8777c97ca1166697ac842f482f787f7cb
Pascal haas <pascal-haas@outlook.fr>
2021-09-20 23:01:59 +0200

    Le pendu FX Dessin du pendu

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -1,17 +1,15 @@
 package model;
+
 import java.util.HashMap;
-
-
 
 public class Joueur {
 
-/**
- * variables
- */
-private int _nombreVies;
-private final HashMap<String, String>_lettresUtilisees;
+    /**
+     * variables
+     */
+    private int _nombreVies;
+    private final HashMap<String, String> _lettresUtilisees;
 
-    
     /**
      * Constructeur
      */
@@ -33,28 +31,46 @@ private final HashMap<String, String>_lettresUtilisees;
         return _nombreVies <= 0;
     }
 
-/**
- * verifie si la lettre a deja ete utilisée
- * @param lettre
- * @return
- */
+    /**
+     * @return int return the _nombreVies
+     */
+    public int get_nombreVies() {
+        return _nombreVies;
+    }
+
+    /**
+     * @param _nombreVies the _nombreVies to set
+     */
+    public void set_nombreVies(int _nombreVies) {
+        this._nombreVies = _nombreVies;
+    }
+
+    /**
+     * verifie si la lettre a deja ete utilisée
+     * 
+     * @param lettre
+     * @return
+     */
     public boolean lettreDejaUtilisee(String lettre) {
         return _lettresUtilisees.containsKey(lettre);
-        
-    }
-/**
- * Ajoute la lettre à la liste des lettres trouvees
- * @param lettre
- */
-public void addLettreUtilisee(String lettre) {
-    _lettresUtilisees.put(lettre, lettre);
-}
 
-/**
- * Enleve une vie au joueur
- */
+    }
+
+    /**
+     * Ajoute la lettre à la liste des lettres trouvees
+     * 
+     * @param lettre
+     */
+    public void addLettreUtilisee(String lettre) {
+        _lettresUtilisees.put(lettre, lettre);
+    }
+
+    /**
+     * Enleve une vie au joueur
+     */
     public void retireUneVie() {
         _nombreVies--;
-         System.out.println("pendu_partie_1.Joueur.retireUneVie() "+_nombreVies);
+        System.out.println("pendu_partie_1.Joueur.retireUneVie() " + _nombreVies);
     }
+
 }
--- src/view/Pendu.java
+++ src/view/Pendu.java
@@ -5,8 +5,10 @@ import javafx.scene.effect.Reflection;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import model.Partie;
 
 public class Pendu extends Parent {
     private final StackPane _pane;
@@ -42,6 +44,7 @@ public class Pendu extends Parent {
         // ajout au pane
         _pane.getChildren().add(rectangle);
         dessinePotence();
+        dessinePendu();
     }
 
     private void dessinePotence() {
@@ -65,14 +68,82 @@ public class Pendu extends Parent {
         l3.setTranslateY(-120); // on la deplace au bon endroit
 
         // pied
-        Line l4 = new Line(0, 0, 100,0);
+        Line l4 = new Line(0, 0, 100, 0);
         l4.setStrokeWidth(4);
         l4.setStroke(Color.WHITE);
         l4.setTranslateX(-60); // on la deplace en x au bon endroit
         l4.setTranslateY(150); // on la deplace en y au bon endroit
 
+        _pane.getChildren().addAll(l1, l2, l3, l4);
 
-        _pane.getChildren().addAll(l1, l2, l3,l4);
+    }
 
+    private void dessinePendu() {
+          int nbrVie = Partie.getinstancePartie().get_monJoueur().get_nombreVies();
+        if (nbrVie < 7) {
+            // La corde
+            Line l = new Line(0, 20, 0, 50);//taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(40); // on la deplace au bon endroit
+            l.setTranslateY(-130);
+            _pane.getChildren().add(l);
+        }
+        if (nbrVie < 6) {
+            // La tête
+            Circle c = new Circle(20);//taille
+            c.setStrokeWidth(4);
+            c.setStroke(Color.WHITE);
+            c.setTranslateX(40); // on la deplace au bon endroit
+            c.setTranslateY(-90);
+            _pane.getChildren().add(c);
+        }
+        if (nbrVie < 5) {
+            // Le corps
+            Line l = new Line(0, 0, 0, 50);//taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(40); // on la deplace au bon endroit
+            l.setTranslateY(-40);
+            _pane.getChildren().add(l);
+        }
+        if (nbrVie < 4) {
+            // Le bras gauche
+            Line l = new Line(10, 0, 60, -40);//taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(15); // on la deplace au bon endroit
+            l.setTranslateY(-40);
+            _pane.getChildren().add(l);
+        }
+        if (nbrVie < 3) {
+            // Le bras droit
+            Line l = new Line(10, -40, 60, 0);//taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(65); // on la deplace au bon endroit
+            l.setTranslateY(-40);
+            _pane.getChildren().add(l);
+        }
+        if (nbrVie < 2) {
+            // La jambe gauche gauche
+            Line l = new Line(10, 0, 30, -80); //taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(30); // on la deplace au bon endroit
+            l.setTranslateY(28);
+            _pane.getChildren().add(l);
+        }
+        if (nbrVie < 1) {
+            // La jambe gauche droite
+            Line l = new Line(10, -80, 30, 0); //taille
+            l.setStrokeWidth(4);
+            l.setStroke(Color.WHITE);
+            l.setTranslateX(50); // on la deplace au bon endroit
+            l.setTranslateY(28);
+            _pane.getChildren().add(l);
+        }
+ 
+        
     }
 }
